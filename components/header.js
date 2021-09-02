b1c4410b765260aa3dadbed7cb3f28a518e9ab54
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-09-01 23:21:53 -0500

    [sm] attached a link to the guide I am using for future reference

--- components/header.js
+++ components/header.js
@@ -1,3 +1,6 @@
+// I used this guide to make this web component
+// https://dev.to/anuradha9712/create-reusable-web-components-in-html-1llc
+
 class Header extends HTMLElement {
     constructor() {
         // Always call super first in constructor
