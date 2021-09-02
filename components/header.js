0d5d2aed15a244210fde26ac723a703b75f3da6c
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-09-01 23:19:01 -0500

    [sm] created a header.js that creates a Header class extending HTMLElement

---
+++ components/header.js
@@ -0,0 +1,7 @@
+class Header extends HTMLElement {
+    constructor() {
+        // Always call super first in constructor
+        super();
+        // write element functionality in here
+    }
+}
