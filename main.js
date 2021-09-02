26ed47f45286dfd8e6ca2bc1a06495fbf01e6437
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-09-01 21:52:08 -0500

    [sm] made css file and attached it

---
+++ main.js
@@ -0,0 +1,23 @@
+myAge = 10;
+
+// a loop to age the user
+for (var i = 0; i < myAge; i++) {
+  console.log(i);
+}
+
+let newPosts = [
+    {
+        title: 'Post One',
+        body: 'This is post one'
+        Date: 'Jan 1, 2019'
+        like: {
+            id: 1,
+            username: 'johndoe',
+            likeType: "heart"
+        }
+    },
+    {
+        title: 'Post Two',
+        body: 'This is post two'
+        Date: 'Jan 1, 2019'
+    },
